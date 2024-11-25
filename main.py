3f74869f56768934a73ea8e53b6c1183b8117623
sigmotoa <si.galvismotoa@gmail.com>
2024-11-25 16:51:21 -0600

    added router in the app

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from router.estancia import estancia
 
 app = FastAPI()
 
@@ -11,3 +12,6 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+app.include_router(estancia)
