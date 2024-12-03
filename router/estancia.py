f174868284903f55f9628db8221f8a270b8c37b8
sigmotoa <si.galvismotoa@gmail.com>
2024-12-03 13:22:10 -0600

    Just one Item returned added

--- router/estancia.py
+++ router/estancia.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path
 
 from data.WebUAA import COURSE_DATA
 
@@ -9,3 +9,12 @@ estancia = APIRouter()
 def show_content_list():
     titles = [{"id": item.id, "title":item.title} for item in COURSE_DATA]
     return titles
+
+
+@estancia.get("/lists/{item_id}")
+def show_one_content(item_id:int=Path(...)):
+    for item in COURSE_DATA:
+        if item.id==item_id:
+            return item
+
+    return {"message":"Item not found"}
