a2b834ecbe8d7d57e417c2ed49e4d13da4ec405f
sigmotoa <si.galvismotoa@gmail.com>
2024-12-10 20:48:23 -0600

    Item3,4 added

--- data/WebUAA.py
+++ data/WebUAA.py
@@ -18,5 +18,23 @@ COURSE_DATA = [
             CourseContent(topic="Github is a Service", description="Git is a tool, and github is a service.", resources=["https://www.geeksforgeeks.org/difference-between-git-and-github/"])
 
         ]
+    ),
+    CourseItem(
+        id=3,
+        title="First steps in GIT",
+        key_concepts=[
+            CourseContent(topic="Download", description="Go to the git website and download and install based in your OS.", resources=["https://git-scm.com/downloads/"]),
+            CourseContent(topic="Install", description="Click next in the window until finish."),
+            CourseContent(topic="Setting Up", description="In the Command Line add your name and email. Is mandatory to sign your commits.", resources=["https://git-scm.com/book/ms/v2/Getting-Started-First-Time-Git-Setup"]),
+
+
+        ]
+    ),
+    CourseItem(
+        id=4,
+        title="Your first local repo",
+        key_concepts=[
+            CourseContent(topic="Choose or create an empty folder", description="In an empty folder viewed from CLI run: ´git init .´ ", resources=["https://git-scm.com/book/ms/v2/Git-Basics-Getting-a-Git-Repository"])
+        ]
     )
 ]
