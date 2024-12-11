bc330b58c31637319900fc43410abca5dd2567e5
sigmotoa <si.galvismotoa@gmail.com>
2024-12-10 22:10:52 -0600

    Server improve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, APIRouter
 from router.estancia import estancia
-
+import requests
+import uvicorn
+from apscheduler.schedulers.background import BackgroundScheduler
 app = FastAPI()
 
 
@@ -13,5 +15,17 @@ async def root():
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
+@app.on_event("startup") # Use on_event to start after FastAPI
+def start_background_tasks():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(keep_awake, 'interval', minutes=8) # trigger every 8 minutes
+    scheduler.start()
+def keep_awake():
+    try:
+        response = requests.get("http://127.0.0.1:8000/")  # Ping your root endpoint
+        response.raise_for_status()  # Raise an exception for bad status codes (4xx or 5xx)
+        print(f"Ping successful: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"Ping failed: {e}")
 
 app.include_router(estancia)
